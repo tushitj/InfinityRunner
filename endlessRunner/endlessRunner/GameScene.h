275478e74ddd8954c29320681ff0e3bfb9521827
Tushit Jain <tushitjain@Tushits-MacBook-Air.local>
2017-03-06 19:27:43 -0500

    Intial commit

---
+++ endlessRunner/endlessRunner/GameScene.h
@@ -0,0 +1,13 @@
+//
+//  GameScene.h
+//  endlessRunner
+//
+//  Created by Tushit Jain on 3/6/17.
+//  Copyright © 2017 Tushit Jain. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameScene : SKScene
+
+@end
